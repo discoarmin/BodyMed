654f309a24ca8951fd4384fbf8de0d8693d5b87f
Armin Brenner <armin.brenner1@freenet.de>
2016-06-17 08:24:55 +0200

    Dateien Für Hauptform-Ereignisse

---
+++ BodyMed/HauptFormEreignisse.cs
@@ -0,0 +1,179 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HauptFormEreignisseErnaehrung.Cs" company="Brenners Videotechnik">
+//   Copyright (c) Brenners Videotechnik. All rights reserved.
+// </copyright>
+// <summary>
+//   Zusammenfassung für HauptForm.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+// <remarks>
+//     <para>Autor: Armin Brenner</para>
+//     <para>
+//        History : Datum     bearb.  Änderung
+//                  --------  ------  ------------------------------------
+//                  14.06.16  br      Grundversion
+//      </para>
+// </remarks>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BodyMed
+{
+    using System;
+    using System.Data;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+
+    using Infragistics.Win.UltraWinGrid;
+
+    using static HauptForm.Formular;
+
+    using Resources = Properties.Resources;
+
+    /// <summary>
+    /// Ereignisse für Blutdruckdaten.
+    /// </summary>
+    [SuppressMessage("ReSharper", "RedundantEmptyDefaultSwitchBranch")]
+    [SuppressMessage("ReSharper", "SwitchStatementMissingSomeCases")]
+    public partial class HauptForm : RibbonForm
+    {
+        /// <summary>Bearbeitet die Aktivierung einer Zelle .</summary>
+        private void UltraGridCellActivated()
+        {
+            // Individuelle Bearbeitung je nach ausgewählter Ansicht
+            if (this.selectedTab == (int)Ernaehrung)
+            {
+                this.blutDruckEinstellen = false;                               // Keine Blutdruckeingabe
+                this.gewichtEinstellen = true;                                  // Gewichtseingabe ist aktiviert
+                this.GetRowIndex();                                             // Index der aktiven Zeile ermitteln
+            }
+            else
+            {
+                this.blutDruckEinstellen = true;                                // Blutdruckeingabe ist aktiviert
+                this.gewichtEinstellen = false;                                 // keine Gewichtseingabe
+                this.GetRowIndex();                                             // Index der aktiven Zeile ermitteln
+            }
+        }
+
+        /// <summary>Bearbeitet die Aktivierung einer Zeile .</summary>
+        /// <param name="e">Die <see cref="EventArgs"/> Instanz, welche die Ereignisdaten enthält.</param>
+        private void UltraGridAfterRowActivate(EventArgs e)
+        {
+            // Individuelle Bearbeitung je nach ausgewählter Ansicht
+            if (this.selectedTab == (int)Ernaehrung)
+            {
+                // Abbrechen, wenn keine gültigen Daten vorhanden sind
+                if (e == EventArgs.Empty || this.bindingManagerGewicht == null)
+                {
+                    return;                                                     // Abbruch, da keine Ereignisdaten übergeben wurden oder keine Verbindungsdaten zur Datenbank vorhanden sind
+                }
+
+                this.blutDruckEinstellen = false;                               // Keine Blutdruckeingabe
+                this.gewichtEinstellen = true;                                  // Gewichtseingabe ist aktiviert
+                this.GetRowIndex();                                             // Index der aktiven Zeile ermitteln
+                this.DisplayRecordNumbers();                                    // Nummer des aktuellen Datensatzes anzeigen ...
+                this.rowPosMerkErnaehrung = this.sliderErnaehrung.Value;        // ... und merken
+            }
+            else
+            {
+                // Abbrechen, wenn keine gültigen Daten vorhanden sind
+                if (e == EventArgs.Empty || this.bindingManagerBlutDruck == null)
+                {
+                    return;                                                     // Abbruch, da keine Ereignisdaten übergeben wurden oder keine Verbindungsdaten zur Datenbank vorhanden sind
+                }
+
+                this.blutDruckEinstellen = true;                                // Blutdruckeingabe ist aktiviert
+                this.gewichtEinstellen = false;                                 // keine Gewichtseingabe
+                this.GetRowIndex();                                             // Index der aktiven Zeile ermitteln
+                this.DisplayRecordNumbers();                                    // Nummer des aktuellen Datensatzes anzeigen ...
+                this.rowPosMerkBlutDruck = this.sliderBlutDruck.Value;          // ... und merken
+            }
+        }
+
+        /// <summary>Bearbeitet das Einfügen einer Zeile .</summary>
+        private void UltraGridAfterRowInsert()
+        {
+            this.auswahllisteLoeschen = true;                                   // Markierte Zeilen eines Grids dürfen neu ermittelt werden
+            string strInsert;                                                   // Variable für das Einfügekommando
+
+
+            // Individuelle Bearbeitung je nach ausgewählter Ansicht
+            if (this.selectedTab == (int)Ernaehrung)
+            {
+                // Es ist die Gewichts-Tabelle angewählt
+                try
+                {
+                    strInsert = "INSERT INTO[Gewicht] ([Datum], [KG], [FM], [FFM], [KW], [BMI])" + " VALUES("
+                                + DateTime.Now + "," + "0.0 , 0.0, 0.0, 0.0,  )";   // leeren Datensatz einfügen
+
+                    this.oleDbDataAdapterGewicht.InsertCommand.CommandText = strInsert;   // Einfügekommando an DataAdapter übergeben
+                    this.oleDbDataAdapterGewicht.InsertCommand.ExecuteNonQuery();         // Einfügen durchführen
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(Resources.HauptForm_UltraGridAfterRowInsert_Fehler_beim_Einfügen_eines_neuen_Datensatzes_in_die_Gewichtstabelle__ + ex.Message,
+                        Resources.HauptForm_UltraGridAfterRowInsert_Einfügefehler,
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+
+                this.RefreshDataSet(ref this.ultraGridErnaehrung, "Gewicht");   // Datensätze im DataAdapter auffrischen
+                this.geberChipInsert = true;                                        // Merker: Datensatz bei den Geberdaten hinzugefügt
+
+                if (!NeuesWerkZeug && !LeeresWerkZeug)
+                {
+                    // Kein neues Werkzeug -> gespeicherten Datensatz anzeigen
+                    var dataRow = chipdaten.ultraDataSourceFlexToolE05.Rows[0];     // Bei den Root-Daten gibt es nur eine Zeile
+                    var childBandChildRows = dataRow.GetChildRows("Sonstige Daten");
+                    dataRow = childBandChildRows[2];                                // Auf Seriennummer positionieren
+                    var snr = dataRow["Geberchip"].ToString();                      // Seriennummer des Geberchips
+                    this.SetzeGeberE05Ds(snr);                                      // Datensatz mit der gefundenen Seriennummer auswählen
+                }
+                else
+                {
+                    this.SetzeGeberE05Ds(" ");                                      // Leeren Datensatz auswählen
+                }
+            }
+            else
+            {
+                // Es ist die Blutdruck-Tabelle angewählt
+                this.bindingManagerBlutDruck.Position = this.sliderBlutDruck.Value; // Datensatzposition der Blutdruckdaten auf die am Schieberegler eingestellte Position setzen
+
+                // Aktive Zeile im Grid ermitteln
+                this.indexNummerAktiveZeile = Convert.ToString(this.dataSetBlutDruck1.Tables["BlutdruckDaten"].Rows[this.bindingManagerBlutDruck.Position]["Index"]);
+                this.LoescheAuswahl(this.ultraGridErnaehrung);                  // Zuerst alle Auswahlen zurücksetzen
+                this.SetzeAuswahl(this.ultraGridErnaehrung);                    // Falls eine aktive Zeile existiert, diese anzeigen
+                this.rowPosMerk = this.sliderErnaehrung.Value;                  // Jetzige Position im Datensatz merken
+            }
+        }
+
+        /// <summary>Bearbeitet das Einfügen einer Zeile .</summary>
+        private void sliderScroll()
+        {
+            // Individuelle Bearbeitung je nach ausgewählter Ansicht
+            if (this.selectedTab == (int)Ernaehrung)
+            {
+                // Es ist die Gewichts-Tabelle angewählt
+                this.bindingManagerGewicht.Position = this.sliderErnaehrung.Value; // Datensatzposition der Gewichtsdaten auf die am Schieberegler eingestellte Position setzen
+
+                // Aktive Zeile im Grid ermitteln
+                this.indexNummerAktiveZeile = Convert.ToString(this.dataSetGewicht1.Tables["Gewicht"].Rows[this.bindingManagerGewicht.Position]["Index"]);
+                this.LoescheAuswahl(this.ultraGridErnaehrung);                  // Zuerst alle Auswahlen zurücksetzen
+                this.SetzeAuswahl(this.ultraGridErnaehrung);                    // Falls eine aktive Zeile existiert, diese anzeigen
+                this.rowPosMerk = this.sliderErnaehrung.Value;                  // Jetzige Position im Datensatz merken
+            }
+            else
+            {
+                // Es ist die Blutdruck-Tabelle angewählt
+                this.bindingManagerBlutDruck.Position = this.sliderBlutDruck.Value; // Datensatzposition der Blutdruckdaten auf die am Schieberegler eingestellte Position setzen
+
+                // Aktive Zeile im Grid ermitteln
+                this.indexNummerAktiveZeile = Convert.ToString(this.dataSetBlutDruck1.Tables["BlutdruckDaten"].Rows[this.bindingManagerBlutDruck.Position]["Index"]);
+                this.LoescheAuswahl(this.ultraGridErnaehrung);                  // Zuerst alle Auswahlen zurücksetzen
+                this.SetzeAuswahl(this.ultraGridErnaehrung);                    // Falls eine aktive Zeile existiert, diese anzeigen
+                this.rowPosMerk = this.sliderErnaehrung.Value;                  // Jetzige Position im Datensatz merken
+            }
+        }
+    }
+}
---
+++ BodyMed/HauptFormErnaehrung.cs
@@ -0,0 +1,127 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HauptFormEreignisseErnaehrung.Cs" company="Brenners Videotechnik">
+//   Copyright (c) Brenners Videotechnik. All rights reserved.
+// </copyright>
+// <summary>
+//   Zusammenfassung für HauptForm.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+// <remarks>
+//     <para>Autor: Armin Brenner</para>
+//     <para>
+//        History : Datum     bearb.  Änderung
+//                  --------  ------  ------------------------------------
+//                  14.06.16  br      Grundversion
+//      </para>
+// </remarks>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BodyMed
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+
+    using Infragistics.Win.UltraWinGrid;
+
+    using Resources = Properties.Resources;
+
+    /// <summary>
+    /// Ereignisse für Blutdruckdaten.
+    /// </summary>
+    [SuppressMessage("ReSharper", "RedundantEmptyDefaultSwitchBranch")]
+    [SuppressMessage("ReSharper", "SwitchStatementMissingSomeCases")]
+    public partial class HauptForm : RibbonForm
+    {
+        /// <summary>
+        /// Behandelt das 'AfterExitEditMode' Ereignis des ultraGridErnaehrung Controls.
+        /// </summary>
+        /// Der Editiermodus im ultraGridMotor wurde beendet
+        private void OnUltraGridErnaehrungAfterExitEditMode(object sender, EventArgs e)
+        {
+            this.AfterExitEditMode(ref this.ultraGridErnaehrung, "Gewicht"); // Änderungen in Datenbank schreiben
+        }
+
+        /// <summary>Behandelt das AfterRowInsert Ereignis des ultraGridErnaehrung Controls.</summary>
+        /// <param name="sender">Die Quelle des Ereignisses.</param>
+        /// <param name="e">Die <see cref="RowEventArgs"/> Instanz, welche die Ereignisdaten enthält.</param>
+        private void OnUltraGridErnaehrungAfterRowInsert(object sender, RowEventArgs e)
+        {
+
+        }
+
+        /// <summary>Behandelt das AfterRowDeleted Ereignis des ultraGridErnaehrung Controls.</summary>
+        /// <param name="sender">Die Quelle des Ereignisses.</param>
+        /// <param name="e">Die <see cref="RowEventArgs"/> Instanz, welche die Ereignisdaten enthält.</param>
+        private void OnUltraGridErnaehrungAfterRowsDeleted(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>Behandelt das AfterSelectChange Ereignis des ultraGridErnaehrung Controls.</summary>
+        /// <param name="sender">Die Quelle des Ereignisses.</param>
+        /// <param name="e">Die <see cref="RowEventArgs"/> Instanz, welche die Ereignisdaten enthält.</param>
+        private void OnUltraGridErnaehrungAfterSelectChange(object sender, AfterSelectChangeEventArgs e)
+        {
+
+        }
+
+        /// <summary>Behandelt das AfterSortChange Ereignis des ultraGridErnaehrung Controls.</summary>
+        /// <param name="sender">Die Quelle des Ereignisses.</param>
+        /// <param name="e">Die <see cref="RowEventArgs"/> Instanz, welche die Ereignisdaten enthält.</param>
+        private void OnUltraGridErnaehrungAfterSortChange(object sender, BandEventArgs e)
+        {
+
+        }
+
+        /// <summary>Behandelt das BeforeCellActivate Ereignis des ultraGridErnaehrung Controls.</summary>
+        /// <param name="sender">Die Quelle des Ereignisses.</param>
+        /// <param name="e">Die <see cref="RowEventArgs"/> Instanz, welche die Ereignisdaten enthält.</param>
+        private void OnUltraGridErnaehrungBeforeCellActivate(object sender, CancelableCellEventArgs e)
+        {
+
+        }
+
+        /// <summary>Behandelt das BeforeRowsDeleted Ereignis des ultraGridErnaehrung Controls.</summary>
+        /// <param name="sender">Die Quelle des Ereignisses.</param>
+        /// <param name="e">Die <see cref="RowEventArgs"/> Instanz, welche die Ereignisdaten enthält.</param>
+        private void OnUltraGridErnaehrungBeforeRowsDeleted(object sender, BeforeRowsDeletedEventArgs e)
+        {
+
+        }
+
+        /// <summary>Behandelt das CellChange Ereignis des ultraGridErnaehrung Controls.</summary>
+        /// <param name="sender">Die Quelle des Ereignisses.</param>
+        /// <param name="e">Die <see cref="RowEventArgs"/> Instanz, welche die Ereignisdaten enthält.</param>
+        private void OnUltraGridErnaehrungCellChange(object sender, CellEventArgs e)
+        {
+
+        }
+
+        /// <summary>Behandelt das InitializeLayout Ereignis des ultraGridErnaehrung Controls.</summary>
+        /// <param name="sender">Die Quelle des Ereignisses.</param>
+        /// <param name="e">Die <see cref="RowEventArgs"/> Instanz, welche die Ereignisdaten enthält.</param>
+        private void OnUltraGridErnaehrungInitializeLayout(object sender, InitializeLayoutEventArgs e)
+        {
+
+        }
+
+        /// <summary>Behandelt das InitializePrint Ereignis des ultraGridErnaehrung Controls.</summary>
+        /// <param name="sender">Die Quelle des Ereignisses.</param>
+        /// <param name="e">Die <see cref="RowEventArgs"/> Instanz, welche die Ereignisdaten enthält.</param>
+        private void OUltraGridErnaehrungInitializePrint(object sender, CancelablePrintEventArgs e)
+        {
+
+        }
+
+        /// <summary>Behandelt das Leave Ereignis des ultraGridErnaehrung Controls.</summary>
+        /// <param name="sender">Die Quelle des Ereignisses.</param>
+        /// <param name="e">Die <see cref="RowEventArgs"/> Instanz, welche die Ereignisdaten enthält.</param>
+        private void OnUltraGridErnaehrungLeave(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
